70fc56d462738544712c6f443b6100f4be019047
man <a@b.ru>
2023-08-16 20:25:54 +0300

    List Slicing

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 # Списки
-
-array = []  # пустой список
-array2 = list('Python')  # пустой список через функцию list
-array3 = [1, 2, 3, 4, 5]  # аналог маcсива типа int
-list_4 = [36.6, 50, 'Температура', True]
-index = -1
-length = len('Python')
-print('Список длиной -', length)
-print(list_4[-3])
-print(array2)
-
-counter = 0  # счётчик элементов
-
-while counter < len(list_4):
-    print(list_4[counter])
-    counter += 1
-
 # Срез подчиняется системе sss
-# s - start
-# s - stop
-# s - step
+# s - start (включительно), по умолчанию - ноль
+# s - stop (не включительно)
+# s - step, по умолчанию - 1
 
-print(list_4[0:len(list_4):2])
+a = ['имя', 'бремя', 'стремя', 'знамя', 'пламя', 'племя']
+print(a[::-1])
