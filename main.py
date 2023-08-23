a909185c551e833e88b8a195a64c18faf2a505a6
man <a@b.ru>
2023-08-23 20:39:38 +0300

    Dictinary Generation From Keys

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 # Словари (dictionary)
-# key - value
-# ключи будут 1. блюдо, ...
-d = {
-    'блюдо': 'dish',
-    'состав': 'composition',
-    'кот': 'cat',
-    'собака': 'dog'
-}
+# key - создание с помощью fromkeys
+d = dict.fromkeys(['one', 'two', 'three'], '1')
 
-print(list(d.keys()))  # список ключей
-print(list(d.values()))  # список значений
-for key, value in d.items():
-    print(key, value, sep='->')
-# выясним есть ли слово "кот" среди ключей
-if 'кот' in d.keys():  # метод keys - ключи
-    print('Кот есть')
-if 'cat' in d.values():  # метод values - значения
-    print('Cat есть')
+print(d)
