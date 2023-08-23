22aaa4bd5681377bac50181ef950f12e51b52d32
man <a@b.ru>
2023-08-23 20:43:23 +0300

    Dictinary Generation From Two Lists #1

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 # Словари (dictionary)
-# key - создание с помощью fromkeys
-d = dict.fromkeys(['one', 'two', 'three'], '1')
+# key - создание с помощью генератора словарей
+# используя два списка
+keys = ['one', 'two', 'three']
+values = ['один', 'два', 'три']
+
+d = dict()  # создан пустой словарь
+l = len(keys)
+
+for i in range(l):
+    d[keys[i]] = values[i]
 
 print(d)
