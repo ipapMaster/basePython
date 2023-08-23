ca680ca881ee97e8f67fa11ec09d31de2b012ed3
man <a@b.ru>
2023-08-23 20:18:20 +0300

    Dictinary Declare and Output in Loop #3

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 # Словари (dictionary)
 # key - value
-
+# ключи будут 1. блюдо, 2. состав
 d = {
     'блюдо': 'окрошка',
-    'состав': ['квас', 'яйцо', 'огурец', 'колбаса']
+    'состав': ['квас', 'яйцо', 'огурец', 'колбаса'],
+    'кот': 'Барсик',
+    'кушает': ['Wiskas', 'Purina']
 }
 
-for item in d:
+for item in d:  # перебор словаря по ключам
     if isinstance(d[item], list):
-        d[item].sort()
+        d[item].sort()  # сортируем список перед выводом
         print(item, end=':\n')
-        for i, v in enumerate(d[item]):
-            print('\t' + str(i + 1) + '.', v)
+        for index, value in enumerate(d[item]):
+            print('\t' + str(index + 1) + '.', value)
     else:
         print(item, d[item], sep=': ')
