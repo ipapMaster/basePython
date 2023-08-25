52aef4be4f1526aa876bd96fdd1eedee2285ee23
man <a@b.ru>
2023-08-25 21:28:33 +0300

    Recursion

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-# Анонимная функция (lambda)
+# Рекурсия на примере факториала
 
 def power(a, b):
     return a ** b
+
+
+def factorial(x):
+    if x == 1:
+        return x
+    return x * factorial(x - 1)
 
 
 lst = ['Иван', 'Пётр', 'Сидор']
@@ -11,3 +17,4 @@ lst2 = map(lambda a: 'Привет, ' + a + '!!!', lst)
 
 print(lst)
 print(list(lst2))
+print(factorial(5))
