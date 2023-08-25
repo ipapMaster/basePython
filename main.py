1d898abb2a39a71035f66060cdf96223a8a7630f
man <a@b.ru>
2023-08-25 20:55:22 +0300

    Type Annotation

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
-# Функции с переменным число аргументов
-def var_args(*args):
-    """
-    :param args: только целые числа,
-    :return: кортеж - число аргументов и сумму как целое число
-    """
-    summ = 0
-    for arg in args:
-        summ += arg
-    return len(args), summ, args
+# Аннотирование типов
+
+def summ(a: int, b: int) -> int:
+    return a + b
 
 
-def named_args(**kwargs):
-    print(kwargs)
-
-
-def any_func_refer(n):
-    n[1] = 0
-    print(n)
-
-
-qnt, summa, lst = var_args(1, 2, 3, 4, 5)
-print('Число аргументов:', qnt)
-print('Сумма:', summa)
-print('Числа на входе:', *lst)
-
-# вызов named_args (key-word args)
-named_args(first='первый', second='второй', third='третий')
-
-array = [5, 25, 625]
-any_func_refer(array)
+print(summ(4, 6))
