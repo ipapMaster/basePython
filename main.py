19aeaef7a92bea28982400f8bd61f1cbbce9a14d
man <a@b.ru>
2023-08-25 20:48:08 +0300

    Args and kwargs

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-# Функции
-def greeting(name='', surname='', age=0, sex=''):
-    # local scope
-    string = 'Привет, ' + str(name) + ' ' + str(surname) + '!'
-    if age != 0:
-        string += '\nТебе ' + str(age) + ' лет!'
-    if sex:
-        string += '\nПол ' + sex
-    return string
+# Функции с переменным число аргументов
+def var_args(*args):
+    """
+    :param args: только целые числа,
+    :return: кортеж - число аргументов и сумму как целое число
+    """
+    summ = 0
+    for arg in args:
+        summ += arg
+    return len(args), summ, args
 
 
-def incr():
-    # local scope
-    global num
-    num += 1
-    return num
+def named_args(**kwargs):
+    print(kwargs)
 
 
-def even_or_odd(number):
-    if number % 2 == 0:
-        return 'Чётное'
-    return 'Нечётное'
+def any_func_refer(n):
+    n[1] = 0
+    print(n)
 
 
-# global scope
-num = 7  # глобальная переменная
-print(even_or_odd(num))
+qnt, summa, lst = var_args(1, 2, 3, 4, 5)
+print('Число аргументов:', qnt)
+print('Сумма:', summa)
+print('Числа на входе:', *lst)
 
+# вызов named_args (key-word args)
+named_args(first='первый', second='второй', third='третий')
+
+array = [5, 25, 625]
+any_func_refer(array)
