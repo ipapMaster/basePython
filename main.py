fa427a69a2e24919989e49e435c0a5f12ea27774
man <a@b.ru>
2023-08-25 18:47:23 +0300

    Set Introduction

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-# ДЗ - обучаемый словарь
-d = {
-    'стол': 'table',
-    'стул': 'chair'
-}
-
-while True:
-    word = input('Введите слово на русском или q - для выхода: ')
-    if word == 'q' or word == 'Q':
-        break
-    if word in d.keys():
-        print('Перевод слова', word, '-', d.get(word))
-    else:
-        transl = input('Этот перевод я не знаю, введите и я его запомню: ')
-        d[word] = transl
-print('До свидания!')
+lst = ['3', '1', '5', '3', '2', '4', '3', '2', '5', '3', '4']
+num_set = set(lst)
+print(num_set)
