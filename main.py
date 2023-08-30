efe758553e5736072870dc1254cf3040fd3a0614
man <a@b.ru>
2023-08-30 19:46:18 +0300

    Print Into File

--- main.py
+++ main.py
@@ -4,10 +4,6 @@
 # режим(mode) - w (write) - файл либо создаётся,
 # либо в нём все стирается и пишется заново
 # a (append) - дописывается (если не существовал, создаётся)
-line = ''
-
-f = open('info.txt', 'rt', encoding='utf-8')
-for _ in range(3):
-    line += f.readline()  # построчное чтение 3х строк
-print(line)
+f = open('info.txt', 'wt', encoding='utf-8')
+print('Печатаем прямо в файл', file=f)  # печать прямо в файл
 f.close()
