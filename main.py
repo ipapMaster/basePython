3b279fdd9e22d2c0ee28ed945a3aa0cf67215661
man <a@b.ru>
2023-08-30 19:32:03 +0300

    File Read by Some Bytes

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # RAM - Random Access Memory (ОЗУ)
 # SSD - Solid State Drive (Flash Memory, ПЗУ)
 # filename + .ext - расширение
-# режим(mode) - w (write) - файл создаётся, в нём все
-# стирается и пишется заново
-# a (append) - дописывается
+# режим(mode) - w (write) - файл либо создаётся,
+# либо в нём все стирается и пишется заново
+# a (append) - дописывается (если не существовал, создаётся)
 
-f = open('info.txt', 'at', encoding='utf-8')
-bytes = f.write('Эта информация уже дописана\n')
-print('В файл записано', bytes, 'байт.')
+f = open('info.txt', 'rt', encoding='utf-8')
+info = f.read(3)  # без аргумента читает файл от начала и до конца
+print('Из файла прочитано:')
+print(info)
+f.read(12)  # чтение без сохранения в переменную
+info = f.read(3)
+print('Из файла также прочитано:')
+print(info)
 f.close()
