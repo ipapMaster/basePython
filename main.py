0fd69c311e81eb9148d215855de7324acd0d246f
man <a@b.ru>
2023-09-01 21:32:19 +0300

    Weather

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-# Исключения
 # Модули и их применение
-# Утверждение (Assertion)
-try:
-    text = input('Введите любой текст: ')
-    assert len(text) > 3  # утверждение
-except AssertionError:
-    print('Длина текста меньше 3-х символов')
+import requests
+
+key = 'c747bf84924be997ff13ac5034fa3f86'
+city = input('Введите город: ')
+
+# ссылка, с которой мы получим все данные в формате JSON
+url = 'http://api.openweathermap.org/data/2.5/weather'
+# Дополнительные парамтеры (Ключ, город введенный пользователем и единицины измерения - metric означает Цельсий)
+params = {'APPID': key, 'q': city, 'units': 'metric'}
+
+result = requests.get(url, params=params)
+res = result.json()
+# print(res)
+print('Температура:', res['main']['temp'], '°C')
+print('Реально ощущается как:', res['main']['feels_like'], '°C')
