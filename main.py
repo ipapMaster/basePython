2c4f12f2863fa8ba08459c01826d72d2c6fc7417
man <a@b.ru>
2023-09-08 20:22:35 +0300

    OOP

--- main.py
+++ main.py
@@ -1,24 +1,100 @@
-# Работа с БД PostgreSQL
-# pip install psycopg2
-import psycopg2 as db
+# При наследовании:
+# базовый, родительский, суперкласс, super
+# производным, дочерним, derived
 
-# Соединение
-connection = db.connect(host='localhost',
-                        port=5500,
-                        user='postgres',
-                        password='123',
-                        dbname='biblio')
-# Курсор
-cursor = connection.cursor()
+def shape_info(shape):
+    print(f'Фигура - {shape.name}. Площадь = {shape.area()}, Периметр = {shape.perimetr()}')
 
-query = "SELECT * FROM genre"
-cursor.execute(query)
-result = cursor.fetchall()
 
-print(result[1][0])
+class Fruit:
+    count = 0  # static
 
-# Отключаем курсор
-cursor.close()
+    def __init__(self, name='noname', weight=0):
+        self.name = name
+        self.weight = weight
+        Fruit.count += 1
 
-# Отключаемся от БД
-connection.close()
+    def get_name(self):
+        return self.name
+
+    def set_name(self, newname):
+        self.name = newname
+
+    @staticmethod
+    def get_count():
+        return Fruit.count
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        self.name = 'Круг'
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+    def perimetr(self):
+        return 2 * 3.14 * self.radius
+
+
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def area(self):
+        return self.a * self.b
+
+    def perimetr(self):
+        return (self.a + self.b) * 2
+
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+        self.name = 'Квадрат'
+
+
+class Student:
+    def __init__(self, name, university):
+        self.name = name
+        self.university = university
+
+
+class Employee:
+    def __init__(self, name, company):
+        self.name = name
+        self.company = company
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+# создаём экземпляры
+circle = Circle(5)
+square = Square(5)
+men = [
+    Student('Bill', 'Oxford'),
+    Employee('Tom', 'Google'),
+    Person('Bob')
+]
+
+# Работаем с экземплярами
+shape_info(circle)
+shape_info(square)
+
+for man in men:
+    if isinstance(man, Student):
+        print(man.university)
+    elif isinstance(man, Employee):
+        print(man.company)
+    else:
+        print(man.name)
+
+apple = Fruit()
+lemon = Fruit()
+mango = Fruit()
+
+print(Fruit.get_count())
